87f179a9b3179cb0b84b53f30752a78e9840bb5b
raveenaw_IT19146652 <it19146652@my.sliit.lk>
2022-05-11 10:14:48 +0530

    Database connection implemented

--- server.js
+++ server.js
@@ -10,4 +10,26 @@ app.use(cors());
 app.use(body_parser.json());
 
 const PORT = process.env.PORT || 8089;
-const MONGODB_URI = " "
+const MONGODB_URI = process.env.MONGODB_URL;
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, (error) =>{
+  if(error){
+    console.log("Database error", error.message);
+  }
+});
+
+
+mongoose.connection.once('open', () =>{
+  console.log('Database Synced');
+});
+
+app.listen(PORT, () =>{
+  console.log(`API is up and running on PORT ${PORT}`);
+});
+
+app.route('/').get((req, res) => {
+  res.send('CTSE Customer Management Application Backend: Microservices');
+});
