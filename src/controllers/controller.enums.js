5ac5baccf479b0c88044a7114f9982e90fb42e9a
raveenaw_IT19146652 <it19146652@my.sliit.lk>
2022-05-11 12:34:26 +0530

    Added controller enums

---
+++ src/controllers/controller.enums.js
@@ -0,0 +1,12 @@
+export default {
+  customer: {
+    CREATE_SUCCESS: 'Customer created successfully',
+    CREATE_ERROR: 'Customer creation unsuccess',
+    LOGIN_SUCCESS: 'Customer login success',
+    LOGIN_ERROR: 'Customer login unsuccess',
+    ALREADY_EXIST: 'Customer already exists',
+    NOT_FOUND: 'Customer not found',
+    PASSWORD_NOT_MATCH: 'Password is not matched',
+    CREDENTIAL_REQUIRED: 'Username and the password is required',
+  }
+};
